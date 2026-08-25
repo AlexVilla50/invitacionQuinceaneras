d63e55c9b5e793603e4dc755523d2e6809ed791f
alexander Villa <alexander.villa.restrepo@gmail.com>
2026-08-25 10:53:29 -0500

    Fix relative paths for html assets and update dress code text

--- js/invitacion.js
+++ js/invitacion.js
@@ -389,11 +389,11 @@ const modalContents = {
 
   dresscode: `
     <h3>Traje formal</h3>
-    <p class="dresscode-reserved-text">Se reservan colores fuertes, azul oscurito y azul rey para esta celebración. Te agradecemos elegir otra tonalidad para tu atuendo.</p>
+    <p class="dresscode-reserved-text">Se reservan colores fuertes, azul oscuro y azul rey para esta celebración. Te agradecemos elegir otra tonalidad para tu atuendo.</p>
     <div class="dresscode-modal-card dresscode-modal-single">
       <p class="dresscode-colors-text">Colores recomendados:</p>
       <div class="dresscode-figure dresscode-figure--contain">
-        <img src="img/dressCode.png" alt="Traje formal" loading="lazy">
+        <img src="../img/dressCode.png" alt="Traje formal" loading="lazy">
       </div>
     </div>
   `,
